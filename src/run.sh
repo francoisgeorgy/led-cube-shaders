c5b61173cb71eeedc3f69567cf2038bfd27193ae
François Georgy <francois.georgy@gmail.com>
2024-01-27 21:26:31 +0100

    Use absolute path for shady (because of sudo)

--- src/run.sh
+++ src/run.sh
@@ -4,15 +4,16 @@ export EGL_PLATFORM=surfaceless
 export MESA_GL_VERSION_OVERRIDE=3.3
 
 if [[ -z "$1" ]]; then
-    echo "usage: $0 SHADER-SCRIPT"
+    echo "usage: $0 <SHADER-SCRIPT>"
     exit 1
 fi
 
 LEDCAT="/home/cube/rpi-rgb-led-matrix/examples-api-use/ledcat"
+SHADY="/home/francois/.local/share/go/bin/shady"
 
 script="$1"
 
-shady -ofmt rgb24 -g 192x128 -f 20 -i "${script}" \
+$SHADY -ofmt rgb24 -g 192x128 -f 20 -i "${script}" \
     | sudo $LEDCAT \
         --led-rows=64 --led-cols=64 --led-slowdown-gpio=5 \
         --led-parallel=2 --led-chain=3 --led-brightness=65
